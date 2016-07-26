04d0cc8e3f0204888339d107455b1fdd860cc59a
denny7794 <denny7794@gmail.com>
2016-07-26 11:54:08 +0400

    поменяли в draw методах на круг-предок

--- src/com/denny7794/rgbcircles/ICanvasView.java
+++ src/com/denny7794/rgbcircles/ICanvasView.java
@@ -4,7 +4,7 @@ package com.denny7794.rgbcircles;
  * Created by Denis on 25.07.2016.
  */
 public interface ICanvasView {
-    void drawCircle(MainCircle circle);
+    void drawCircle(SimpleCircle circle);
 
     //void redraw();
 }
