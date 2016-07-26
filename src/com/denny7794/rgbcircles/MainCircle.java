3c359bf95ab2d7964d98d2d7a76ba59b3e34f02a
denny7794 <denny7794@gmail.com>
2016-07-26 11:51:56 +0400

    создали класс-предок всех кругов SimpleCircle

--- src/com/denny7794/rgbcircles/MainCircle.java
+++ src/com/denny7794/rgbcircles/MainCircle.java
@@ -3,29 +3,12 @@ package com.denny7794.rgbcircles;
 /**
  * Created by Denis on 25.07.2016.
  */
-public class MainCircle {
+public class MainCircle extends SimpleCircle{
     public static final int INIT_RADIUS = 20;
     public static final int MAIN_SPEED = 30;
-    private int x;
-    private int y;
-    private int radius;
 
     public MainCircle(int x, int y) {
-        this.x = x;
-        this.y = y;
-        radius = INIT_RADIUS;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public int getRadius() {
-        return radius;
+        super(x, y, INIT_RADIUS);
     }
 
     public void moveMainCircleWhenTouchAt(int x1, int y1) {
---
+++ src/com/denny7794/rgbcircles/SimpleCircle.java
@@ -0,0 +1,28 @@
+package com.denny7794.rgbcircles;
+
+/**
+ * Created by Denis on 26.07.2016.
+ */
+public class SimpleCircle {
+    protected int x;
+    protected int y;
+    protected int radius;
+
+    public SimpleCircle(int x, int y, int radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+}
