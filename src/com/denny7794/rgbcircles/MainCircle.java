49059d502e6159dfe546e311f5ba7e5ceaf9a300
denny7794 <denny7794@gmail.com>
2016-07-26 12:24:28 +0400

    раскрасили кружочки

--- src/com/denny7794/rgbcircles/MainCircle.java
+++ src/com/denny7794/rgbcircles/MainCircle.java
@@ -1,14 +1,18 @@
 package com.denny7794.rgbcircles;
 
+import android.graphics.Color;
+
 /**
  * Created by Denis on 25.07.2016.
  */
 public class MainCircle extends SimpleCircle{
     public static final int INIT_RADIUS = 20;
     public static final int MAIN_SPEED = 30;
+    public static final int OUR_COLOR = Color.BLUE;
 
     public MainCircle(int x, int y) {
         super(x, y, INIT_RADIUS);
+        setColor(OUR_COLOR);
     }
 
     public void moveMainCircleWhenTouchAt(int x1, int y1) {
--- src/com/denny7794/rgbcircles/SimpleCircle.java
+++ src/com/denny7794/rgbcircles/SimpleCircle.java
@@ -7,6 +7,7 @@ public class SimpleCircle {
     protected int x;
     protected int y;
     protected int radius;
+    private int color;
 
     public SimpleCircle(int x, int y, int radius) {
         this.x = x;
@@ -24,5 +25,13 @@ public class SimpleCircle {
 
     public int getRadius() {
         return radius;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
     }
 }
