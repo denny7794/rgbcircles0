96ad0333d3ded8af7260fd1c23d3156d0da13c49
denny7794 <denny7794@gmail.com>
2016-07-27 10:05:32 +0400

    остальные круги движутся при нажатии на экран

--- src/com/denny7794/rgbcircles/EnemyCircle.java
+++ src/com/denny7794/rgbcircles/EnemyCircle.java
@@ -12,9 +12,14 @@ public class EnemyCircle extends SimpleCircle{
     public static final int FROM_RADIUS = 10;
     public static final int TO_RADIUS = 15;
     public static final int ENEMY_COLOR = Color.RED;
+    public static final int RANDOM_SPEED = 5;
+    private int dx;
+    private int dy;
 
-    public EnemyCircle(int x, int y, int radius) {
+    public EnemyCircle(int x, int y, int radius, int dx, int dy) {
         super(x, y, radius);
+        this.dx = dx;
+        this.dy = dy;
     }
 
 
@@ -22,8 +27,10 @@ public class EnemyCircle extends SimpleCircle{
         Random random = new Random();
         int x = random.nextInt(GameManager.getWidth());
         int y = random.nextInt(GameManager.getHeight());
+        int dx = 1 + random.nextInt(RANDOM_SPEED);
+        int dy = 1 + random.nextInt(RANDOM_SPEED);
         int radius = FROM_RADIUS + random.nextInt(TO_RADIUS - FROM_RADIUS);
-        EnemyCircle enemyCircle = new EnemyCircle(x, y , radius);
+        EnemyCircle enemyCircle = new EnemyCircle(x, y , radius, dx, dy);
         return enemyCircle;
     }
 
@@ -40,5 +47,10 @@ public class EnemyCircle extends SimpleCircle{
             return true;
         }
         return false;
+    }
+
+    public void moveOneStep() {
+        x += dx;
+        y += dy;
     }
 }
